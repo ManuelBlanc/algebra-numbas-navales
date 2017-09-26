ae889fcd0f1eae5f1800bd4af6c19bc33e751798
ManuelBlanc <manuel.blanc@estudiante.uam.es>
2017-09-26 23:17:56 +0200

    asdadsad

--- src/algebra-navales.js
+++ src/algebra-navales.js
@@ -26,8 +26,18 @@ Numbas.addExtension('algebra-navales', ['base', 'jme'], function(extension) {
     permutations3[1] = [[1,0,0],[0,0,1],[0,1,0]];
     permutations3[2] = [[0,0,1],[0,1,0],[1,0,0]];
 
-    function randomInt(n) {
-        return Math.floor(n*Math.random());
+    function randomInt(a, b) {
+        switch (arguments.length) {
+            case 1: return     Math.floor(   a *Math.random());
+            case 2: return a + Math.floor((b-a)*Math.random());
+            default: throw new Error("Invalid number of arguments");
+        }
+    }
+
+    function randomNonZeroInt(n) {
+        var r;
+        do r = randomInt(n); while (r === 0);
+        return r;
     }
 
     function swap(A, i, j) {
@@ -37,27 +47,55 @@ Numbas.addExtension('algebra-navales', ['base', 'jme'], function(extension) {
         return A;
     }
 
-    function swapRows(A) {
-        switch (randomInt(3)) {
-            case 0: return swapElems(A, 0, 1);
-            case 1: return swapElems(A, 1, 2);
-            case 2: return swapElems(A, 2, 0);
+    var elementalOps = {};
+
+    elementalOps.swapRows = function(A, i0, i1) {
+        if (i0 === i1) return A;
+        return swap(A, i0, i1);
+    };
+
+    elementalOps.multiplyRowByScalar = function(A, i, k) {
+        A[i].forEach(function(aij, j, row) {
+            row[j] = k*aij;
+        });
+        return A;
+    }
+
+    elementalOps.addRowMultiplied = function(A, i0, i1, k) {
+        if (k === undefined) k = 1;
+        A[i0].forEach(function(ai1j, j, row) {
+            row[j] = ai1j + k*A[i1][j];
+        });
+        return A;
+    }
+
+    extension.invertible_matrix = function(ops) {
+        var A = [[1,0,0], [0,1,0], [0,0,1]];
+        while (ops --> 0) {
+            switch (randomInt(3)) {
+
+                case 0:
+                i0 = randomInt(3);
+                i1 = (i0 + randomInt(2)) % 3;
+                elementalOps.swapRows(A, i0, i1);
+                break;
+
+                case 1:
+                i0 = randomInt(3);
+                k = randomNonZeroInt(-5, 5);
+                elementalOps.multiplyRowByScalar(A, i0, k);
+                break;
+
+                case 2:
+                i0 = randomInt(3);
+                i1 = (i0 + randomInt(2)) % 3;
+                k = randomNonZeroInt(-5, 5);
+                elementalOps.addRowMultiplied(A, i0, i1, k);
+                break;
+            }
         }
-    }
-
-    function multiplyRow(A) {
-        var k;
-        do k = randomInt(10)-5; while (k === 0);
-        A[randomInt(3)].map(function(aij) { return k*aij; });
-    }
-
-    function addRow(A) {
-        var k, srcrow, dstrow;
-        do k = randomInt(10)-5; while (k === 0);
-        var srcrow = randomInt(3);
-        do dstrow = randomInt(3); while (srcrow === distrow);
-        A[dstrow].forEach(function(aij, j, row) { row[j] = aij + A[srcrow][j]; });
-    }
+        return A;
+    };
 
 
     extension.simple_matrix = function(nums) {
@@ -117,34 +155,54 @@ Numbas.addExtension('algebra-navales', ['base', 'jme'], function(extension) {
     function kVar(constant, variable, hideSign) {
         if (k === 0) return '';
         if (k === -1) return '-' + v;
-        if (hideSign && constant < 0) return '+' + constant
-        return k.toString() + v
+        if (hideSign && constant < 0) return '+' + constant;
+        return k.toString() + v;
     }
 
     extension.display_as_system = function(A) {
         var buffer = [];
         buffer.push('\\begin{align}');
-        buffer.push(kVar(A[0][0], 'x', true) '&' + kVar(A[0][1], 'y') + '&' + kVar(A[0][2], 'z') + '& = ' + b[0] + '\\\\');
-        buffer.push(kVar(A[1][0], 'x', true) '&' + kVar(A[1][1], 'y') + '&' + kVar(A[1][2], 'z') + '& = ' + b[1] + '\\\\');
-        buffer.push(kVar(A[2][0], 'x', true) '&' + kVar(A[2][1], 'y') + '&' + kVar(A[2][2], 'z') + '& = ' + b[2] + '\\\\');
+        buffer.push(kVar(A[0][0], 'x', true) + '&' + kVar(A[0][1], 'y') + '&' + kVar(A[0][2], 'z') + '& = ' + b[0] + '\\\\');
+        buffer.push(kVar(A[1][0], 'x', true) + '&' + kVar(A[1][1], 'y') + '&' + kVar(A[1][2], 'z') + '& = ' + b[1] + '\\\\');
+        buffer.push(kVar(A[2][0], 'x', true) + '&' + kVar(A[2][1], 'y') + '&' + kVar(A[2][2], 'z') + '& = ' + b[2] + '\\\\');
         buffer.push('\\end{align}');
+        return buffer.join('');
     };
-
-    function display_col_as_vector(A, col) {
-        return display_as_matrix([[A[0][col]], [A[1][col]], [A[2][col]]]);
-    }
 
     extension.display_as_base = function(A) {
-        var buffer = '';
-        buffer += '\\left(';
-        buffer += display_col_as_vector(A, 0);
-        buffer += display_col_as_vector(A, 1);
-        buffer += display_col_as_vector(A, 2);
-        buffer += '\\right';
-        return buffer;
+        var buffer = [];
+        buffer.push('\\left(');
+        buffer.push(display_as_matrix([[A[0][0]], [A[1][0]], [A[2][0]]]), ', ');
+        buffer.push(display_as_matrix([[A[0][1]], [A[1][1]], [A[2][1]]]), ', ');
+        buffer.push(display_as_matrix([[A[0][2]], [A[1][2]], [A[2][2]]]));
+        buffer.push('\\right');
+        return buffer.join('');
     };
 
+    extension.matrix_equality_percent = function(A, B) {
+        if (A.rows    !== B.rows   ) return 0;
+        if (A.columns !== B.columns) return 0;
+
+        var i, j;
+        var equalCells = 0;
+        for (i=0; i < A.rows; i++) {
+            for (j=0; j < A.columns; j++) {
+                if (A[i][j] === B[i][j]) equalCells++;
+            }
+        }
+
+        return equalCells / (A.rows * A.columns);
+    };
+
+    // Matrix generation
     scope.addFunction(new funcObj('simple_matrix', [TList], TMatrix, extension.simple_matrix, {unwrapValues: true}));
     scope.addFunction(new funcObj('simple_imatrix', [TList], TMatrix, extension.simple_imatrix, {unwrapValues: true}));
+
+    scope.addFunction(new funcObj('simple_imatrix', [TList], TMatrix, extension.simple_imatrix, {unwrapValues: true}));
+
+    // Display
+    scope.addFunction(new funcObj('display_as_base', [TList], TMatrix, extension.simple_imatrix, {unwrapValues: true}));
+    scope.addFunction(new funcObj('display_as_system', [TList], TMatrix, extension.simple_imatrix, {unwrapValues: true}));
+    scope.addFunction(new funcObj('display_as_matrix', [TList], TMatrix, extension.simple_imatrix, {unwrapValues: true}));
 
 });
